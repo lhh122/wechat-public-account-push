78f3b40c83a553fe7ca82b30e4bf7ab802eb61fe
lhh122 <113901171+lhh122@users.noreply.github.com>
2022-09-20 01:04:10 +0800

    Update index.js

--- 配置/index.js
+++ 配置/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "wx8d3582247b5b2474",
+  APP_ID:"wx8d3582247b5b2474",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "b86f03d5c24f8782af71f73c37dc2c06",
+  APP_SECRET:"b86f03d5c24f8782af71f73c37dc2c06",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
